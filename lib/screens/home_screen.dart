62005af48e9aca42ee811527ee84328fb1b7986c
Lucas Freitas de Barros <lucasfreitasdebarros@gmail.com>
2021-04-29 09:02:18 -0300

    Category screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/tabs/category_tab.dart';
 import '../tabs/home_tab.dart';
 import '../widgets/custom_drawer.dart';
 
@@ -15,6 +16,16 @@ class HomeScreen extends StatelessWidget {
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
         ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Categorias"
+            ),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: CategoryTab()
+        )
       ],
     );
   }
